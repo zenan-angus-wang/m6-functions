1de0b1745cb6b17be3532e9f9d2d3f6ca12a26b2
ZENAN WANG <zenanwang@D-108-179-156-22.dhcp4.washington.edu>
2016-10-06 14:39:10 -0700

    finished the programming project

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,15 +1,21 @@
 # Exercise 1: writing and executing functions
 
 # Write a function `AddThree` that adds 3 to an input value
-
+AddThree <- function(n) {
+  answer = n + 3
+  return (answer)
+}
 
 # Create a variable `ten` by passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
-
-
+FeetToMeters <- function(feet) {
+  meter = feet / 3.28
+  return (meter)
+}
 # Create a variable `height.in.feet` that is your height in feet
-
+height.in.feet <- 5.9 #feet 
 
 # Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+height.in.meters <- FeetToMeters(height.in.feet)
